7a41ac93b9886dd4b4c954bab6df70c0bee73b46
Mohammad Shishegar <mohammad127sh82@gmail.com>
2020-09-07 16:08:45 +0430

    Updates

---
+++ precache-manifest.09aa48118055086e0d751f2504b4a239.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768ab8cadf826d63a9d43bda3f81da2b",
+    "url": "/Natours/index.html"
+  },
+  {
+    "revision": "06f6f31d082fbb05c01a",
+    "url": "/Natours/static/css/main.7e0bdd86.chunk.css"
+  },
+  {
+    "revision": "b734b9baed44c164aa55",
+    "url": "/Natours/static/js/2.53c1442d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Natours/static/js/2.53c1442d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f6f31d082fbb05c01a",
+    "url": "/Natours/static/js/main.b159ce52.chunk.js"
+  },
+  {
+    "revision": "d78d480bb53d95d8672f",
+    "url": "/Natours/static/js/runtime-main.df86c672.js"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/Natours/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "4c42c11f1fc0203483c21f2d6eeb8896",
+    "url": "/Natours/static/media/logo-green-1x.4c42c11f.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/Natours/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "d6c15bf0efcaf4c03c5cb0c9078cdd17",
+    "url": "/Natours/static/media/nat-1.d6c15bf0.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/Natours/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "4d3783688636b59662283e19991a2912",
+    "url": "/Natours/static/media/nat-2.4d378368.jpg"
+  },
+  {
+    "revision": "cc2b05433cbad0b369d131d02c0cac6a",
+    "url": "/Natours/static/media/nat-3.cc2b0543.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/Natours/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/Natours/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/Natours/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/Natours/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/Natours/static/media/video.ffcd876c.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa9ca3618a4e4976628daba22d682db4.js"
+  "/Natours/precache-manifest.09aa48118055086e0d751f2504b4a239.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Natours/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
